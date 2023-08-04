055d6450c469358f1006a53332d13c87b2278051
MarkusNodeJs <55816864+MarkusTech@users.noreply.github.com>
2023-08-04 14:26:45 +0800

    error middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,33 +1,29 @@
 import userModel from "../models/userModel.js";
 
 const register = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-    // validate
-    if (!name) {
-      next("name is required");
-    }
-    if (!email) {
-      next("email is required");
-    }
-    if (!password) {
-      next("password is required and greater than 6 character");
-    }
-
-    // validate if the user is existing
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      next("Email Already Exist Please Login");
-    }
-
-    // if not existed user save!
-    const user = await userModel.create({ name, email, password });
-    res
-      .status(201)
-      .send({ success: true, message: "User Registered Successfully", user });
-  } catch (error) {
-    next(error);
+  const { name, email, password } = req.body;
+  // validate
+  if (!name) {
+    next("name is required");
   }
+  if (!email) {
+    next("email is required");
+  }
+  if (!password) {
+    next("password is required and greater than 6 character");
+  }
+
+  // validate if the user is existing
+  const existingUser = await userModel.findOne({ email });
+  if (existingUser) {
+    next("Email Already Exist Please Login");
+  }
+
+  // if not existed user save!
+  const user = await userModel.create({ name, email, password });
+  res
+    .status(201)
+    .send({ success: true, message: "User Registered Successfully", user });
 };
 
 const login = async (req, res) => {
